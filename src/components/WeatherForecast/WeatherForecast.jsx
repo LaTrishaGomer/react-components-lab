ec39c306a0b5e4ea1b20addb0c91a281d70ec723
latrisha <l_gomer@yahoo.com>
2025-03-27 08:17:38 -0700

    finished assignment

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,16 @@
+import WeatherIcon from './WeatherIcon';
+import WeatherData from './WeatherData';
+import './WeatherForecast.css'; 
+
+const WeatherForecast = ({ day, img, imgAlt, conditions, time }) => {
+  return (
+    <div className="weather">
+      <WeatherData day={day} conditions={conditions} time={time} />
+      <WeatherIcon img={img} imgAlt={imgAlt} />
+    </div>
+  );
+};
+
+export default WeatherForecast;
+
+  
